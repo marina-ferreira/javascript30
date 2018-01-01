30dd2bb36a6f63e24455f68f97d42b60d6417c5b
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-01 21:42:05 -0200

    [flex-image-gallery] Fresh code to apply flexbox solution

--- 05_flex-image-gallery/scripts.js
+++ 05_flex-image-gallery/scripts.js
@@ -1,26 +1,3 @@
 window.onload = () => {
   var panels = document.querySelectorAll('.panel');
-
-  panels.forEach((panel) => panel.addEventListener('mouseover', slideUp));
-  panels.forEach((panel) => panel.addEventListener('mouseleave', slideDown));
-}
-
-function slideUp(e) {
-  var panelTitle = this.querySelector('.title');
-  var details = this.querySelector('.details');
-
-  if (!panelTitle || !details) return;
-
-  panelTitle.style = 'transform: translateY(150px); visibility: visible';
-  details.style = 'margin-bottom: 60px; visibility: visible';
-}
-
-function slideDown(e) {
-  var panelTitle = this.querySelector('.title');
-  var details = this.querySelector('.details');
-
-  if (!panelTitle || !details) return;
-
-  panelTitle.style = 'transform: translateY(-150px); visibility: hidden; opacity: 0';
-  details.style = 'margin-bottom: -250px; visibility: hidden; opacity: 0';
 }
