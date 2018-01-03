89d210505cf88a8200a31a8e50e249c62ffe206d
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-03 12:28:38 -0200

    [cardio-#2] Solution implementation

--- 07_cardio#2-solution/scripts.js
+++ 07_cardio#2-solution/scripts.js
@@ -17,12 +17,31 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+let isAdult = people.some(person => new Date().getFullYear() - person.year > 18);
+console.log({isAdult});
+
 // Array.prototype.every() // is everyone 19 or older?
+let allAdult = people.every(person => new Date().getFullYear() - person.year > 18);
+console.log({allAdult});
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+let comment = comments.find(comment => comment.id === 823423);
+console.log({comment});
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+let commentIndex = comments.findIndex(comment => comment.id === 823423);
+
+// Creates a copy of comments array without the commentIndex element
+let newComments = [
+  ...comments.slice(0, commentIndex),
+  ...comments.slice(commentIndex + 1)
+];
+
+console.table(newComments);
+
+comments.splice(commentIndex, 1); // Changes the comments array itself
+console.table(comments);
