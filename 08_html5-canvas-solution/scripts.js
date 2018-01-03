3567055edb16f34aca4620bef9c10de8bb4a93b2
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-03 20:39:53 -0200

    [html5-canvas] Adds hue and width update in draw function

--- 08_html5-canvas-solution/scripts.js
+++ 08_html5-canvas-solution/scripts.js
@@ -1,15 +1,16 @@
 window.onload = () => {
   const canvas = document.querySelector('#draw');
   const context = canvas.getContext('2d');
-  let isDrawing = false;
-  let lastX, lastY;
+  let hue = 0,
+      isDrawing = false,
+      direction = true,
+      lastX, lastY;
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  context.strokeStyle = '#BADA55';
   context.lineJoin = 'round';
   context.lineCap = 'round';
-  context.lineWidth = 100;
+  context.lineWidth = 50;
 
   function setInitialCoordinates(e) {
     isDrawing = true;
@@ -19,12 +20,22 @@ window.onload = () => {
   function draw(e) {
     if (!isDrawing) { return; }
 
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     context.beginPath();
     context.moveTo(lastX, lastY);
     context.lineTo(e.offsetX, e.offsetY);
     context.stroke();
 
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    hue = hue % 360;
+
+    if (context.lineWidth >= 100 || context.lineWidth <= 1) {
+      direction = !direction;
+    }
+
+    direction ? context.lineWidth++ : context.lineWidth--;
   }
 
   canvas.addEventListener('mousedown', setInitialCoordinates);
