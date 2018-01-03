5e39a2740a1bf1872f8c353b0716c5b0f994197b
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-03 19:36:14 -0200

    [html5-canvas] Canvas draw continuous line

--- 08_html5-canvas/scripts.js
+++ 08_html5-canvas/scripts.js
@@ -1,38 +1,31 @@
 window.onload = () => {
-  window.isMouseUp = true;
-  canvas.addEventListener('mousedown', getOrigin);
-  canvas.addEventListener('mouseup', getOrigin);
-  canvas.addEventListener('mousemove', getCoordinates);
-}
-
-getOrigin = (e) => {
-  window.isMouseUp = e.type == 'mouseup';
-
-  window.prevX = e.clientX;
-  window.prevY = e.clientY;
-}
-
-function getCoordinates(e) {
-  if (window.isMouseUp) return;
-
-  let x = e.clientX;
-  let y = e.clientY;
-  console.log(window.prevX, window.prevY, x, y);
-
-  draw(window.prevX, window.prevY, x, y);
-
-  window.prevX = x;
-  window.prevY = y;
-}
-
-  draw = (prevX, prevY, x, y) => {
+  let isMouseUp = true;
   let canvas = document.querySelector('#canvas');
-  let context = canvas.getContext('2d');
+  let x, y;
 
-  context.beginPath();
-  context.lineJoin = "round";
-  context.moveTo(window.prevX, window.prevX);
-  context.lineTo(x, y);
-  context.closePath();
-  context.stroke();
+  getClickPosition = (e) => {
+    isMouseUp = e.type == 'mouseup';
+
+    x = e.clientX;
+    y = e.clientY;
+  }
+
+  draw = (e) => {
+    if (isMouseUp) return;
+
+    let context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.lineJoin = "round";
+    context.moveTo(x, y);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.stroke();
+
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+
+  canvas.addEventListener('mousedown', getClickPosition);
+  canvas.addEventListener('mouseup', getClickPosition);
+  canvas.addEventListener('mousemove', draw);
 }
