f5821a5c2983f1548d87e303fa53ffea483df054
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-12 15:30:25 -0200

    [array-sort] Sort names ignoring articles script

--- 17_array-sort/scripts.js
+++ 17_array-sort/scripts.js
@@ -1,9 +1,22 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+  'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean',
+  'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans',
+  'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
 
-let bandList = document.querySelector('.band-list');
+const articlesRegex = new RegExp(/\bthe\b|\ban\b|\ba\b/, 'gi'),
+      bandList = document.querySelector('.band-list');
 
-bandList.innerHTML = bands.map(band => {
-  return `
-    <li class="band">${band}</li>
-  `;
+let sortedBands = bands.sort((a, b) => {
+  let strippedA = a.replace(articlesRegex, ' ').trim(),
+      strippedB = b.replace(articlesRegex, ' ').trim();
+
+  if (strippedA < strippedB) return -1;
+  if (strippedA > strippedB) return 1;
+
+  return 0;
+});
+
+bandList.innerHTML = sortedBands.map(band => {
+  return `<li class="band">${band}</li>`;
 }).join('');
