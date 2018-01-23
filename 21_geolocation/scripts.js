6d8c4e1227ed7a76d9f8da5a4134eac71e63e924
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-23 19:40:30 -0200

    [geolocation] Solution implementation

--- 21_geolocation/scripts.js
+++ 21_geolocation/scripts.js
@@ -1,8 +1,9 @@
-navigator.geolocation.getCurrentPosition(position => {
+let speedTag = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(position => {
   let heading = position.coords.heading || 255,
-      speed = position.coords.speed || 27.93,
-      speedTag = document.querySelector('.speed');
+      speed = position.coords.speed || 27.93;
 
   document.documentElement.style.setProperty('--heading', `${heading}deg`);
-  speedTag.textContent = `${speed} km/h`;
-});
+  speedTag.textContent = speed;
+}, (err) => console.err(err));
