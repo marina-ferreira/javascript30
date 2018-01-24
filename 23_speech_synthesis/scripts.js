0eddc1d45e67ba9b89653ad5a3045349a896ca3b
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-24 16:00:40 -0200

    [speech-synthesis] Solution implementation

--- 23_speech_synthesis/scripts.js
+++ 23_speech_synthesis/scripts.js
@@ -1,51 +1,37 @@
 let select = document.querySelector('select.voices'),
-    form = document.querySelector('form'),
-    pauseButton = document.querySelector('.pause'),
+    startButton = document.querySelector('.start'),
+    stopButton = document.querySelector('.stop'),
+    textarea = document.querySelector('textarea'),
+    ranges = document.querySelectorAll('input[type="range"]'),
     synth = window.speechSynthesis,
+    text = new SpeechSynthesisUtterance(textarea.value),
     voices = [];
 
 function populateVoiceList() {
   voices = synth.getVoices();
 
-  voices.forEach(voice => {
-    let option = document.createElement('option');
-
-    option.value = voice.name;
-    option.textContent = `${voice.name} --- ${voice.lang}`;
-    select.append(option);
-  });
+  select.innerHTML = voices.map(voice => {
+    return `<option value="${voice.name}">${voice.name} - ${voice.lang}</option>`;
+  }).join('');
 }
 
-function speak(e) {
-  e.preventDefault();
+function setVoice() {
+  let voiceName = select.selectedOptions[0].value;
+  text.voice = voices.find(voice => voice.name === voiceName);
+}
 
-  let textarea = document.querySelector('textarea'),
-      pitch = document.querySelector('input[name="pitch"]'),
-      rate = document.querySelector('input[name="rate"]'),
-      text = new SpeechSynthesisUtterance(textarea.value),
-      voiceName = select.selectedOptions[0].value;
+function setOptions(e) {
+  text[this.name] = this.value;
+  toggleSpeak();
+}
 
-  text.voice = voices.filter(voice => voice.name === voiceName)[0] || null;
-  text.pitch = pitch.value;
-  text.rate = rate.value;
+function toggleSpeak(startOver = true) {
   synth.cancel();
-  synth.speak(text);
+  startOver && synth.speak(text);
 }
 
-function togglePause(e) {
-  if (synth.paused) {
-    synth.resume();
-    pauseButton.value = 'Pause';
-  } else {
-    synth.pause();
-    pauseButton.value = 'Resume';
-  }
-}
-
-populateVoiceList();
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = populateVoiceList;
-}
-
-form.addEventListener('submit', speak);
-pauseButton.addEventListener('click', togglePause);
+speechSynthesis.addEventListener('voiceschanged', populateVoiceList);
+startButton.addEventListener('click', toggleSpeak);
+stopButton.addEventListener('click', toggleSpeak.bind(null, false));
+ranges.forEach(range => range.addEventListener('change', setOptions));
+select.addEventListener('change', setVoice);
