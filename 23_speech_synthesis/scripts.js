6345199505dac46190682d995ca6aa49d1c61286
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-24 14:07:49 -0200

    [speech-synthesis] Scripts for text synthesis: choose voice, speak, pause, resume

---
+++ 23_speech_synthesis/scripts.js
@@ -0,0 +1,47 @@
+let select = document.querySelector('select.voices'),
+    form = document.querySelector('form'),
+    pauseButton = document.querySelector('.pause'),
+    synth = window.speechSynthesis,
+    voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  voices.forEach(voice => {
+    let option = document.createElement('option');
+
+    option.value = voice.name;
+    option.textContent = `${voice.name} --- ${voice.lang}`;
+    select.append(option);
+  });
+}
+
+function speak(e) {
+  e.preventDefault();
+
+  let textarea = document.querySelector('textarea'),
+      text = new SpeechSynthesisUtterance(textarea.value),
+      voiceName = select.selectedOptions[0].value;
+
+  text.voice = voices.filter(voice => voice.name === voiceName)[0] || null;
+  synth.cancel();
+  synth.speak(text);
+}
+
+function togglePause(e) {
+  if (synth.paused) {
+    synth.resume();
+    pauseButton.value = 'Pause';
+  } else {
+    synth.pause();
+    pauseButton.value = 'Resume';
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+form.addEventListener('submit', speak);
+pauseButton.addEventListener('click', togglePause);
