219b7150cbdfba0099dc1a50e0b46b117dde29f7
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-24 18:21:45 -0200

    [sticky-nav] Solution

---
+++ 24_sticky-nav-solution/scripts.js
@@ -0,0 +1,14 @@
+let nav = document.querySelector('#main'),
+    topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', fixNav);
