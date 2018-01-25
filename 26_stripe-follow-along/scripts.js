d37807d4a316ad9ec916a0d46aaea0617036d4ce
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-25 18:51:26 -0200

    [stripe-follow-along] Populate dropdown

--- 26_stripe-follow-along/scripts.js
+++ 26_stripe-follow-along/scripts.js
@@ -8,10 +8,27 @@ function placeDropdown(e) {
       offsetX = this.offsetLeft + (this.offsetWidth - dropdown.offsetWidth) / 2;
 
   !isActive && dropdown.classList.add('active');
+  populateDropdown.call(this);
 
   dropdown.style.transform = `translateX(${offsetX}px)`;
   dropdown.style.transition = `${styleProperty} .2s ease-in-out`;
 }
 
+function populateDropdown() {
+  let content = document.querySelector(`.submenu.${this.dataset.name}`),
+      contentHeight = content.getBoundingClientRect().height,
+      clone = content.cloneNode(true);
+
+  dropdown.style.height = `${contentHeight}px`;
+  dropdown.innerHTML = '';
+  dropdown.append(clone);
+
+  clone.style.visibility = 'visible';
+}
+
+function removeDropdown() {
+  dropdown.classList.remove('active');
+}
+
 links.forEach(link => link.addEventListener('mouseenter', placeDropdown));
-menu.addEventListener('mouseleave', () => dropdown.classList.remove('active'));
+menu.addEventListener('mouseleave', removeDropdown);
