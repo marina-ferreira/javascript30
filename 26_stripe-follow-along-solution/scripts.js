14ce3089a49c6c8347c7feb3f476c79821e02f7d
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-01-27 23:03:55 -0200

    [stripe-follow-along] Solution - Scripts for moving dropdown

---
+++ 26_stripe-follow-along-solution/scripts.js
@@ -0,0 +1,32 @@
+const triggers = document.querySelectorAll('.cool > li'),
+      background = document.querySelector('.dropdownBackground'),
+      nav = document.querySelector('.top');
+
+function handleEnter() {
+  this.classList.add('trigger-enter');
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+  background.classList.add('open');
+
+  let dropdown = this.querySelector('.dropdown'),
+      dropdownCoords = dropdown.getBoundingClientRect(),
+      navCoords = nav.getBoundingClientRect(),
+      coords = {
+        width: dropdownCoords.width,
+        height: dropdownCoords.height,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
+      };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('top', `${coords.top}px`);
+  background.style.setProperty('left', `${coords.left}px`);
+}
+
+function handleLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
