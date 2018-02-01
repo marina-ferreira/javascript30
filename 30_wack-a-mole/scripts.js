a935d9f6834abf685bb612f88c73fd868b3b3742
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-02-01 19:03:01 -0200

    [wack-a-mole] Scripts for scoring points

--- 30_wack-a-mole/scripts.js
+++ 30_wack-a-mole/scripts.js
@@ -1,30 +1,32 @@
-const start = document.querySelector('button[name="start"]');
+const start = document.querySelector('.controls button[name="start"]');
 const moles = document.querySelectorAll('.mole');
 const timer = document.querySelector('.timer');
+const score = document.querySelector('.controls .score span');
 
-let secs;
-let moleMax = moles.length;
-let standMin = 0.5;
-let standMax = 2;
+let playTime;
 let countdown;
 
 function startGame() {
-  secs = 10;
+  playTime = 10;
+  score.textContent = 0
   clearInterval(countdown);
 
   countdown = setInterval(() => {
-    if (secs < 0) {
+    if (playTime < 0) {
       clearInterval(countdown);
       return;
     }
 
-    timer.textContent = secs;
-    secs--;
+    timer.textContent = playTime;
+    playTime--;
     showMoles();
   }, 1000);
 }
 
 function showMoles() {
+  let moleMax = moles.length;
+  let standMin = 0.5;
+  let standMax = 1.5;
   let randomMole = Math.floor(Math.random() * moleMax);
   let randomStand = (Math.random() * (standMax - standMin) + standMin).toFixed(2) * 1000;
 
@@ -38,5 +40,11 @@ function showMoles() {
   }, randomStand);
 }
 
+function scorePoints() {
+  score.textContent = parseInt(score.textContent) + 1;
+  this.style.setProperty('transform', `translateY(140px)`);
+  this.style.setProperty('opacity', 0);
+}
 
 start.addEventListener('click', startGame);
+moles.forEach(mole => mole.addEventListener('click', scorePoints));
