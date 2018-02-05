454f12a86f6e83ff42760b447e84389bb948a242
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-02-05 17:28:40 -0200

    [portfolio] Css variables - scripts improvements

--- exercises/03_css-variables/scripts.js
+++ exercises/03_css-variables/scripts.js
@@ -1,11 +1,9 @@
-window.onload = function() {
-  document.addEventListener('input', updateImage);
-}
-
 function updateImage(e) {
-  var inputKind = e.target.id;
-  var suffix = inputKind === 'color' ? '' : 'px';
-  var cssValue = `${e.target.value}${suffix}`
+  let inputKind = e.target.id,
+      suffix = inputKind === 'color' ? '' : 'px',
+      cssValue = `${e.target.value}${suffix}`;
 
   document.documentElement.style.setProperty(`--${inputKind}`, cssValue);
 }
+
+document.addEventListener('input', updateImage);
