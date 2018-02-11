f8ed5a17cfcd54dfff592368c87be745127337ec
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-02-11 11:11:17 -0200

    [portfolio] Event Handling - Template structure

--- exercises/25_event-handling/scripts.js
+++ exercises/25_event-handling/scripts.js
@@ -1,11 +1,12 @@
-let divs = document.querySelectorAll('div');
+let divs = document.querySelectorAll('.example div');
 
 function logText(e) {
-  console.log(this.classList.value);  // logs all thre classes due to event bubbling
+  console.log(this.classList.value);  // logs all the classes due to event bubbling
   e.stopPropagation(); // stop bubbling
 }
 
 divs.forEach(div => div.addEventListener('click', logText, {
   capture: true, // run the function on the way down
   once: true // same as calling div.removeEventListener(...)
-}))
+}));
+// document.querySelector('body').addEventListener('click', logText);
